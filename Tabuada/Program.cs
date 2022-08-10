ee19eaefab5f8355c538485ef8915764e4a0bd5b
Henrique Greghi <henriquevsg@gmail.com>
2022-08-10 15:46:59 -0300

    Tabuada

---
+++ Tabuada/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Entre com o número : ");
+            int x = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i<= 10; i++)
+            {
+                a = i * x;
+                Console.WriteLine(i + " x "+ x + " = " + a );
+            }
+        }
+    }
+}
